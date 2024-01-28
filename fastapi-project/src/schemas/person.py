3262ccadb1589a7befa6b6b640bf5fd8f70c6ea9
Maxim Isaev <isaev_0602@mail.ru>
2024-01-28 13:28:08 +0500

    person uuid field type changed to UUID4

--- fastapi-project/src/schemas/person.py
+++ fastapi-project/src/schemas/person.py
@@ -1,4 +1,5 @@
 from pydantic.main import BaseModel
+from pydantic.types import UUID4
 
 
 class PersonSchema(BaseModel):
@@ -6,6 +7,6 @@ class PersonSchema(BaseModel):
     Response model for Person object.
     This class contains info we return to a user.
     """
-    uuid: str
+    uuid: UUID4
     full_name: str
     films: list | None
